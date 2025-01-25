337a55bde427d0c3a4625f599e39c88636e4e9df
Eugene Zhulenev <ezhulenev@google.com>
2025-01-25 11:39:14 -0800

    [xla:cpu] Move elemental kernel emitter into separate folder under codegen
    PiperOrigin-RevId: 719683054

--- third_party/xla/xla/backends/cpu/codegen/elemental/elemental_kernel_emitter.cc
+++ third_party/xla/xla/backends/cpu/codegen/elemental/elemental_kernel_emitter.cc
@@ -13,13 +13,12 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#include "xla/backends/cpu/codegen/elemental_kernel_emitter.h"
+#include "xla/backends/cpu/codegen/elemental/elemental_kernel_emitter.h"
 
 #include <cstddef>
 #include <cstdint>
 #include <memory>
 #include <optional>
-#include <string>
 #include <utility>
 #include <vector>
 
